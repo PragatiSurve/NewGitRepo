5c22b3cd1bd7ea31a3cd31537ffcf95e40941442
Pragati <pragatisurve143@gmail.com>
2023-07-21 09:49:39 +0530

    First commit

---
+++ src/main/java/APITestAutomationTest/APITestAutomationTest/App.java
@@ -0,0 +1,13 @@
+package APITestAutomationTest.APITestAutomationTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
